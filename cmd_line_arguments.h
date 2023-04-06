ca2cdc759e45227cfc5e417e14b48ec7a04cac0a
Prabhu Singh Khalsa <prabhu@lanl.gov>
2023-04-06 14:44:06 -0600

    DOC: Adding doc strings to cmd_line_arguments.h

--- cmd_line_arguments.h
+++ cmd_line_arguments.h
@@ -4,7 +4,7 @@
 #define __CMD_LINE_ARGUMENTS_H_INCLUDED__
 
 /*!
-   This class collects and verifies command line options and creates the help menu
+   This class collects and verifies command line options and creates the help menu.
 */
 class CmdLineArguments {
     public:
@@ -17,20 +17,20 @@ class CmdLineArguments {
         CmdLineArguments (int &argc, char **argv);
         //! String of verbose arguments.
         /*!
-          This function returns a string listing the values of all the command line options
+          This function returns a string listing the values of all the command line options.
           \return The string of command line options
         */
         std::string verboseArguments ();
         //! String with help message.
         /*!
-          This function returns a POSIX-like string with the help message for the user
-          \sa CmdLineArguments()
+          This function returns a string with a POSIX-like help message for the user.
           \return The help message
+          \sa CmdLineArguments()
         */
         std::string helpMessage ();
-        //! Check if string ends with substring
+        //! Check if string ends with substring.
         /*!
-          This function takes in string and a potential substring and says whether the string ends with the provided substring
+          This function takes in two strings and determines whether one string contains the other string as a substring at the end.
           \param full_string full string to check
           \param ending substring to check
           \return Boolean with answer
@@ -38,17 +38,39 @@ class CmdLineArguments {
         bool endsWith (std::string const &full_string, std::string const &ending);
 
         // Getter Functions
+        // \sa is for "see also"
 
-        //! Operator which allows clas to be used like a map (dictionary)
+        //! Operator which allows class to be used like a map data structure.
         /*!
-          This function a command line option corresponding to the provided string
+          This function a command line option corresponding to the provided string.
           \param str command line option
           \return Value of command line option
+          \sa CmdLineArguments()
         */
         std::string operator[](std::string const &str) const;
+        //! Return string with complete command line given.
+        /*!
+          Return the complete command line used by user.
+          \return String with all command line options given
+        */
         const std::string& commandLine() const;
+        //! Return the name of the command given.
+        /*!
+          This returns the value of the command typed on the command line, should be the name of the compiled code where main resides (i.e. argv[0]).
+          \return Name of command
+        */
         const std::string& commandName() const;
+        //! Return the value of the verbose flag.
+        /*!
+          If the user specifies verbose this function will return true, otherwise it will return false. This is a getter method.
+          \return Boolean indicating whether the verbose option is used
+        */
         bool verbose() const;
+        //! Return the value of the help flag.
+        /*!
+          If the user requests the help menu, a flag is set, this function returns the value of that flag. This is a getter method.
+          \return Boolean indicating whether the help option is used
+        */
         bool help() const;
 
     private:
