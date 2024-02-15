31f1fc47b9bda0a1065429ce50070ca3e3a35925
Prabhu Singh Khalsa <prabhu@lanl.gov>
2024-02-14 17:29:11 -0700

    WIP: Using the history output name as the hdf5 group name rather than an incremented number

--- spade_object.cpp
+++ spade_object.cpp
@@ -1640,7 +1640,7 @@ void SpadeObject::write_history_regions(H5::H5File &h5_file, const string &group
         write_history_point(h5_file, history_region_group_name, history_region.point);
         H5::Group history_outputs_group = h5_file.createGroup((history_region_group_name + "/historyOutputs").c_str());
         for (int i=0; i<history_region.historyOutputs.size(); i++) {
-            string history_output_group_name = history_region_group_name + "/historyOutputs/" + to_string(i);
+            string history_output_group_name = history_region_group_name + "/historyOutputs/" + history_region.historyOutputs[i].name;
             write_history_output(h5_file, history_output_group_name, history_region.historyOutputs[i]);
         }
         history_region.historyOutputs = vector<history_output_type>();  // set the vector to an empty vector - i.e. clear the memory
