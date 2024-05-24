a37262d4891e764fd987d2af5e41e10829dccc91
Kyle Brindley <kbrindley@lanl.gov>
2024-05-24 12:10:27 -0600

    WIP: simplify the system test construction because waves level complication is not necessary. Reports direct string in pytest results to make troubleshooting easier

--- spade/tests/test_systemtest.py
+++ spade/tests/test_systemtest.py
@@ -1,8 +1,5 @@
 import os
 import shlex
-import string
-import typing
-import pathlib
 import tempfile
 import subprocess
 from importlib.metadata import version, PackageNotFoundError
@@ -34,36 +31,24 @@ if not installed:
 
 system_tests = [
     # CLI sign-of-life and help/usage
-    [f"{spade_command} --help"],
-    [f"{spade_command} docs --help"],
+    f"{spade_command} --help",
+    f"{spade_command} docs --help",
     # Tutorials
 ]
 if installed:
     system_tests.append(
         # The HTML docs path doesn't exist in the repository. Can only system test from an installed package.
-        [f"{spade_command} docs --print-local-path"]
+        f"{spade_command} docs --print-local-path"
     )
 
 
 @pytest.mark.systemtest
-@pytest.mark.parametrize("commands", system_tests)
-def test_run_tutorial(commands: typing.Iterable[str]) -> None:
-    """Fetch and run the tutorial configuration file(s) as system tests in a temporary directory
+@pytest.mark.parametrize("command", system_tests)
+def test_run_tutorial(command: str) -> None:
+    """Run the system tests in a temporary directory
 
-    Iterates on the command strings in the commands list. Performs string template substitution using keys:
-
-    * ``spade_command``
-    * ``temp_directory``
-
-    :param commands: list of command strings for the system test
+    :param command: command string for the system test
     """
     with tempfile.TemporaryDirectory() as temp_directory:
-        template_substitution = {
-            "spade_command": spade_command,
-            "temp_directory": temp_directory
-        }
-        for command in commands:
-            if isinstance(command, string.Template):
-                command = command.substitute(template_substitution)
-            command = command.split(" ")
-            subprocess.check_output(command, env=env, cwd=temp_directory).decode("utf-8")
+        command = shlex.split(command)
+        subprocess.check_output(command, env=env, cwd=temp_directory).decode("utf-8")
