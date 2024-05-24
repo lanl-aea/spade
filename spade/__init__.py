186a180d9cd500bda3ad546f5c712521bc1bb93b
Kyle Brindley <kbrindley@lanl.gov>
2024-05-24 11:05:40 -0600

    WIP: fix namespace

--- spade/__init__.py
+++ spade/__init__.py
@@ -9,6 +9,7 @@ except PackageNotFoundError:
     except ImportError:
         # Should only hit this when running as an un-installed package in the local repository
         import pathlib
+        import warnings
         warnings.filterwarnings(action='ignore', message='tag', category=UserWarning, module='setuptools_scm')
         import setuptools_scm
         __version__ = setuptools_scm.get_version(root=pathlib.Path(__file__).parent.parent)
