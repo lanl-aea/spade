9cebc451a81d9f3bb63507abcebf01601f89b576
Kyle Brindley <kbrindley@lanl.gov>
2024-12-06 14:03:17 -0700

    WIP: add pass through abaqus command option

--- spade/conftest.py
+++ spade/conftest.py
@@ -11,8 +11,20 @@ def pytest_addoption(parser):
         default=None,
         help="system test build directory root",
     )
+    parser.addoption(
+        "--abaqus-command",
+        action="store",
+        type=pathlib.Path,
+        default=None,
+        help="Abaqus command for system test CLI pass through",
+    )
 
 
 @pytest.fixture
 def system_test_directory(request):
     return request.config.getoption("--system-test-dir")
+
+
+@pytest.fixture
+def abaqus_command(request):
+    return request.config.getoption("--abaqus-command")
