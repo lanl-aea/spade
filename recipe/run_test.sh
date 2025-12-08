22301278b6f55c3fa948ee84ef4451ac0523eb0b
Kyle Brindley <kbrindley@lanl.gov>
2025-12-08 10:32:23 -0700

    TST: match conda-recipe abaqus system tests to developer tests

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,4 +1,4 @@
 system_test_directory=$PWD
 cd $SP_DIR/$PKG_NAME
 pytest -n 4 -vvv -m "not require_third_party" --system-test-dir=${system_test_directory}
-pytest -n 4 -v --no-showlocals -m "systemtest and require_third_party" --tb=short --system-test-dir=${system_test_directory} --abaqus-command=/apps/abaqus/Commands/abq2024
+pytest -n 4 -v --no-showlocals -m "systemtest and require_third_party" --tb=short --system-test-dir=${system_test_directory} --abaqus-command=/apps/abaqus/Commands/abq2024 --abaqus-command=/apps/abaqus/Commands/abq2023 --abaqus-command=/apps/abaqus/Commands/abq2022
