1e9bfa64e933b3fd043f23b62a58ec1676a1b0f0
Kyle Brindley <kbrindley@lanl.gov>
2025-12-08 12:23:46 -0700

    WIP: accept a list of abaqus commands

--- spade/conftest.py
+++ spade/conftest.py
@@ -1,9 +1,12 @@
+"""Define the project's custom pytest options and CLI extensions."""
+
 import pathlib
 
 import pytest
 
 
 def pytest_addoption(parser):
+    """Add the custom pytest options to the pytest command-line parser."""
     parser.addoption(
         "--system-test-dir",
         action="store",
@@ -13,18 +16,25 @@ def pytest_addoption(parser):
     )
     parser.addoption(
         "--abaqus-command",
-        action="store",
-        type=pathlib.Path,
+        action="append",
         default=None,
         help="Abaqus command for system test CLI pass through",
     )
 
 
 @pytest.fixture
-def system_test_directory(request):
+def system_test_directory(request: pytest.FixtureRequest) -> pathlib.Path:
+    """Return the argument of custom pytest ``--system-test-dir`` command-line option."""
     return request.config.getoption("--system-test-dir")
 
 
 @pytest.fixture
-def abaqus_command(request):
-    return request.config.getoption("--abaqus-command")
+def abaqus_command(request: pytest.FixtureRequest) -> pathlib.Path:
+    """Return the argument of custom pytest ``--abaqus-command`` command-line option.
+
+    Returns an empty list if the getopts default ``None`` is provided (no command line argument specified)
+    """
+    command_list = request.config.getoption("--abaqus-command")
+    if command_list is None:
+        command_list = []
+    return command_list
