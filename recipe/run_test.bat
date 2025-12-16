ac92cd9880666856ef06ec9af9da05b82a479604
Kyle Brindley <kbrindley@lanl.gov>
2025-12-16 15:30:57 -0700

    WIP: internal recipe that runs the full system test suite on linux

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,3 +1,3 @@
 set system_test_directory=%CD%
 cd %SP_DIR%\%PKG_NAME%
-pytest -vvv -n 4 -m "not require_third_party" --system-test-dir=%system_test_directory%
+pytest -n 4 -vvv -m "not require_third_party" --system-test-dir=%system_test_directory%
